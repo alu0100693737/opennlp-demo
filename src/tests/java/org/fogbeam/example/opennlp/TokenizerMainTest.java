991296959a81c8fb72188fd210ed30e1fb61c283
Iván García <alu0100693737@ull.edu.es>
2018-05-20 16:43:00 +0200

    Intentando tests y travis por decima vez

---
+++ src/tests/java/org/fogbeam/example/opennlp/TokenizerMainTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package org.fogbeam.example.opennlp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ivan
+ *
+ */
+public class TokenizerMainTest {
+
+
+	@Test
+	public void test1() {
+		assertEquals(2, 2);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() {
+		System.out.println("Setup");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() {
+		System.out.println("tearDown");
+	}
+
+	/**
+	 * Test method for {@link org.fogbeam.example.opennlp.TokenizerMain#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		System.out.println("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		
+	}
+
+}
